7008890034ba88c71dec4e45c6e6fb1c67050e43
chikies11 <you@example.com>
2025-12-07 02:24:08 +0300

    fix with Gemini #2

--- src/main/java/com/yogabot/YogaBotApplication.java
+++ src/main/java/com/yogabot/YogaBotApplication.java
@@ -4,11 +4,12 @@ import com.yogabot.service.SupabaseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.annotation.PostConstruct;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableScheduling
 public class YogaBotApplication {
 
